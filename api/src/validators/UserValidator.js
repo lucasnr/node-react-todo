58a7f1b6cef42da890386a72745598c51a9b2fc9
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-18 15:45:27 -0300

    Fix validation

--- api/src/validators/UserValidator.js
+++ api/src/validators/UserValidator.js
@@ -11,13 +11,16 @@ module.exports = {
 	}),
 	index: celebrate({
 		[Segments.QUERY]: Joi.object().keys({
-			page: Joi.number(),
-			size: Joi.number().greater(0),
+			page: Joi.number().integer(),
+			size: Joi.number().integer().greater(0),
 		}),
 	}),
 	find: celebrate({
 		[Segments.PARAMS]: Joi.object().keys({
-			id: Joi.string().required(),
+			id: Joi.string()
+				.required()
+				.regex(/^[0-9a-fA-F]{24}$/)
+				.error(() => new Error('id must me in a valid pattern')),
 		}),
 	}),
 };
