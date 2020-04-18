b0f227ac25f22bf1884355b9a8e88ca3c5fe7875
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-18 15:52:41 -0300

    Exclude password from select

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
 	name: String,
 	email: String,
-	password: String,
+	password: { type: String, select: false },
 	avatar_url: String,
 });
 
