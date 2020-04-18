05e9596ea67bfb4643e46840dad253bb2bd4debc
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-18 15:29:07 -0300

    Fix double slash issue

--- api/src/controllers/utils/urlBuilder.js
+++ api/src/controllers/utils/urlBuilder.js
@@ -1,1 +1,4 @@
-module.exports = (req, url) => `${req.protocol}://${req.get('host')}/${url}`;
+module.exports = (req, url) => {
+	if (url.startsWith('/')) url = url.slice(1);
+	return `${req.protocol}://${req.get('host')}/${url}`;
+};
