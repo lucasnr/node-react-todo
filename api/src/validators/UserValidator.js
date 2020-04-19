16886c0b24d738d51fe1304411e7736bc99cb4f4
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-19 15:05:36 -0300

    Fix required user's fields

--- api/src/validators/UserValidator.js
+++ api/src/validators/UserValidator.js
@@ -3,18 +3,18 @@ const { celebrate, Segments, Joi } = require('celebrate');
 const JoiMongoId = require('./utils/JoiMongoIdValidator');
 
 module.exports = {
-	store: celebrate({
-		[Segments.BODY]: Joi.object().keys({
-			name: Joi.string().required().min(4),
-			email: Joi.string().required().email(),
-			password: Joi.string().required().min(8),
-			avatar_url: Joi.string().required(),
-		}),
-	}),
 	index: celebrate({
 		[Segments.QUERY]: Joi.object().keys({
 			page: Joi.number().integer(),
 			size: Joi.number().integer().greater(0),
+		}),
+	}),
+	store: celebrate({
+		[Segments.BODY]: Joi.object().keys({
+			name: Joi.string().min(4),
+			email: Joi.string().required().email(),
+			password: Joi.string().required().min(8),
+			avatar_url: Joi.string(),
 		}),
 	}),
 	find: celebrate({
