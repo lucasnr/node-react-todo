d258183e03f73bc302ac63a14dcbbc7771b22082
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-20 17:54:16 -0300

    Configure web app environment

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
+const baseURL = process.env.REACT_APP_API_URL;
 const api = axios.create({
-	baseURL: 'http://localhost:3333',
+	baseURL,
 });
 
 const headers = () => ({
